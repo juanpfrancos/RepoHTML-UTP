305c7e3f59fabe27ed0d535f4fef6234cc5813dc
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2022-03-05 17:49:47 -0500

    Add actions icons.

--- tables/scripts.js
+++ tables/scripts.js
@@ -16,14 +16,27 @@ fetch('https://rickandmortyapi.com/api/character')
     let nameC = document.createElement("td");
     let statusC= document.createElement("td");
     let specieC = document.createElement("td");
+
+    /*Add Action Icons */
+    let action = document.createElement("td");
+    action.setAttribute("id",'idAction_'+character.id)
+    let actionD = document.createElement("i");
+    actionD.setAttribute("class","fa-solid fa-circle-xmark")
+    let actionConfig = document.createElement("i");
+    actionConfig.setAttribute("class","fa-solid fa-gear")
+    action.appendChild(actionD)
+    action.appendChild(actionConfig)
+
     id.textContent = character.id
     nameC.textContent = character.name
     statusC.textContent = character.status
     specieC.textContent = character.species
+
     document.getElementById('id_'+character.id).appendChild(id);
     document.getElementById('id_'+character.id).appendChild(nameC);
     document.getElementById('id_'+character.id).appendChild(statusC);
     document.getElementById('id_'+character.id).appendChild(specieC);
+    document.getElementById('id_'+character.id).appendChild(action);
   })
 })
 
